d4cac9bffb357a16c39275d85f9159187f368e67
Casey Manning <kcgrind@gmail.com>
2025-04-24 16:09:34 -0700

    add terraform db

--- main.tf
+++ main.tf
@@ -86,3 +86,74 @@ resource "aws_eip" "app_eip" {
   instance = aws_instance.app_server.id
   vpc      = true
 }
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.77.0"
+
+  name                 = "alliance"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_db_subnet_group" "alliance" {
+  name       = "alliance"
+  subnet_ids = module.vpc.public_subnets
+
+  tags = {
+    Name = "alliance"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name   = "alliance_rds"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alliance_rds"
+  }
+}
+
+resource "aws_db_parameter_group" "alliance" {
+  name   = "alliance"
+  family = "postgres14"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+}
+
+resource "aws_db_instance" "alliance" {
+  identifier             = "alliance"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "14.1"
+  username               = "edu"
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.alliance.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  parameter_group_name   = aws_db_parameter_group.alliance.name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+}
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,21 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.app_server.public_ip
 }
+
+output "rds_hostname" {
+  description = "RDS instance hostname"
+  value       = aws_db_instance.alliance.address
+  sensitive   = true
+}
+
+output "rds_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.alliance.port
+  sensitive   = true
+}
+
+output "rds_username" {
+  description = "RDS instance root username"
+  value       = aws_db_instance.alliance.username
+  sensitive   = true
+}
