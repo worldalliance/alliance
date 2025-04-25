5abe0af2dddc39b3f39c6b765427bae1a806df05
Casey Manning <kcgrind@gmail.com>
2025-04-25 16:04:01 -0700

    set db password env var

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(
+    'DB_PASSWORD:',
+    typeof process.env.DB_PASSWORD,
+    process.env.DB_PASSWORD ? '[exists]' : '[missing]',
+  );
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT ?? 3005, '0.0.0.0');
