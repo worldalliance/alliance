7c70d40c6e4f14c189679f042205fc9a87250c54
Casey Manning <kcgrind@gmail.com>
2025-04-26 17:21:47 -0700

    test deploy admin app

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
 import { ActionsModule } from './actions/actions.module';
