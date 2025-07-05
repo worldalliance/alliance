19acdc014467f7ecf1024a45bfad90cd201ac9ec
Casey Manning <kcgrind@gmail.com>
2025-07-04 18:01:22 -0700

    fix headers being smaller than body text

--- apps/frontend/src/components/MarkdownWrapper.tsx
+++ apps/frontend/src/components/MarkdownWrapper.tsx
@@ -12,35 +12,35 @@ const MarkdownWrapper: React.FC<MarkdownWrapperProps> = ({
     <div className="markdown-wrapper">
       <ReactMarkdown
         components={{
-          h1: ({ node, ...props }) => (
+          h1: ({ ...props }) => (
             <h1
               className="font-newsreader !font-semibold !text-xl"
               {...props}
             />
           ),
-          h2: ({ node, ...props }) => (
+          h2: ({ ...props }) => (
             <h2
-              className="font-newsreader !font-semibold !text-lg !mt-4"
+              className="font-newsreader !font-semibold !text-xl !mt-4"
               {...props}
             />
           ),
-          p: ({ node, ...props }) => (
+          p: ({ ...props }) => (
             <p className="font-newsreader !text-xl my-4" {...props} />
           ),
-          ol: ({ node, ...props }) => (
+          ol: ({ ...props }) => (
             <ol
               className="font-newsreader !text-xl list-decimal list-inside my-4 pl-4"
               {...props}
             />
           ),
-          ul: ({ node, ...props }) => (
+          ul: ({ ...props }) => (
             <ul
               className="font-newsreader !text-xl list-disc list-inside my-4 pl-4"
               {...props}
             />
           ),
-          li: ({ node, ...props }) => <li className="my-2" {...props} />,
-          a: ({ node, ...props }) => (
+          li: ({ ...props }) => <li className="my-2" {...props} />,
+          a: ({ ...props }) => (
             <a
               className="text-blue-600 hover:text-blue-800 underline"
               {...props}
