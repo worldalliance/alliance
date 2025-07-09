c7abfd786eff97c7c9059d3613cd31112438ad22
Casey Manning <kcgrind@gmail.com>
2025-07-09 16:37:06 -0700

    remove homepage subcomponents

--- apps/frontend/src/pages/app/HomePage.tsx
+++ apps/frontend/src/pages/app/HomePage.tsx
@@ -1,8 +1,7 @@
-import { HomeTaskView } from "../../components/HomeTaskView";
 import { actionsComplete, UserActionDto } from "@alliance/shared/client";
-import { HomeNewActionsView } from "../../components/HomeNewActionsView";
-import GatheringCommitmentsView from "../../components/GatheringCommitmentsView";
 import { getLoadedActionData, RouteMatches } from "../../applayout";
+import TaskCard from "../../components/TaskCard";
+import ActionItemCard from "../../components/ActionItemCard";
 
 const HomePage = ({ matches }: RouteMatches) => {
   const { actions, relations } = getLoadedActionData(matches);
@@ -49,13 +48,62 @@ const HomePage = ({ matches }: RouteMatches) => {
       <div className="flex flex-col py-16 max-w-[728px] md:min-w-[600px] gap-y-5 overflow-y-auto px-3">
         <div className="flex flex-col gap-y-8">
           {todoActions.length > 0 && (
-            <HomeTaskView
-              actions={todoActions.slice(0, 2)}
-              onTaskComplete={handleTaskComplete}
-            />
+            <div className="flex flex-col gap-y-4">
+              <div className="flex flex-row items-center gap-x-2">
+                <p className="font-semibold text-sm text-zinc-500">
+                  Awaiting Completion
+                </p>
+                <span className="inline-flex items-center justify-center w-5 h-5 text-xs font-medium text-zinc-500 bg-zinc-200 rounded-full">
+                  {todoActions.slice(0, 2).length}
+                </span>
+              </div>
+              {todoActions.slice(0, 2).map((action) => (
+                <TaskCard
+                  key={action.id}
+                  action={action}
+                  onComplete={handleTaskComplete}
+                />
+              ))}
+            </div>
           )}
-          <HomeNewActionsView actions={newActions} />
-          <GatheringCommitmentsView actions={committedActions} />
+          {newActions.length > 0 && (
+            <div className="flex flex-col gap-y-4">
+              <div className="flex flex-row items-center gap-x-2">
+                <p className="font-semibold text-sm text-zinc-500">
+                  Awaiting Commitment
+                </p>
+                <span className="inline-flex items-center justify-center w-5 h-5 text-xs font-medium text-zinc-500 bg-zinc-200 rounded-full">
+                  {newActions.length}
+                </span>
+              </div>
+              {newActions.map((action) => (
+                <ActionItemCard
+                  key={action.id}
+                  {...action}
+                  showDescription={true}
+                />
+              ))}
+            </div>
+          )}
+          {committedActions.length > 0 && (
+            <div className="flex flex-col gap-y-4">
+              <div className="flex flex-row items-center gap-x-2">
+                <p className="font-semibold text-sm text-zinc-500">
+                  Still gathering commitments
+                </p>
+                <span className="inline-flex items-center justify-center w-5 h-5 text-xs font-medium text-zinc-500 bg-zinc-200 rounded-full">
+                  {committedActions.length}
+                </span>
+              </div>
+              {committedActions.map((action) => (
+                <ActionItemCard
+                  key={action.id}
+                  {...action}
+                  showDescription={false}
+                />
+              ))}
+            </div>
+          )}
           {/* <InviteMemberCard /> */}
         </div>
       </div>
