adc0e9cf830b2b4f9e569e331ef51b08875489a7
Charles Lien <charleslien97@gmail.com>
2026-01-15 11:55:28 -0800

    feat: add away pills

--- server/src/actions/actions.service.ts
+++ server/src/actions/actions.service.ts
@@ -91,6 +91,8 @@ import {
 import { ShareUrlDto, ShareUrlStatsDto } from './dto/share-url.dto';
 import { Relations } from 'src/utils/Repository';
 import { run } from 'src/utils/promise';
+import { CachedFilter } from 'src/utils/cached-filter';
+import { computeIsAwayDuringAnyOfLastMemberAction } from 'src/utils/action-user';
 
 export class UserActionRelationDto {
   @ApiProperty({ enum: UserActionRelation, enumName: 'UserActionRelation' })
@@ -2133,6 +2135,7 @@ export class ActionsService {
     // --- end of promise defs ---
 
     const now = new Date();
+    const userCachedFilter = new CachedFilter(await usersP);
     const actions = await actionsP;
 
     const allMembersTagId = await allMembersTagIdP;
@@ -2184,9 +2187,6 @@ export class ActionsService {
 
     // Initialize defaults if no action was taken
     for (const action of actions) {
-      for (const userId of await userIdsP) {
-        getDetail({ userId, actionId: action.id });
-      }
       for (const userId of (await joinedUsersP)[action.id]) {
         const detail = getDetail({ userId, actionId: action.id });
         detail.status = action.optional
@@ -2195,6 +2195,18 @@ export class ActionsService {
               action.latestMemberActionEvent.deadline < now
             ? UserActionRelationPillStatus.MissedDeadline
             : UserActionRelationPillStatus.Todo;
+      }
+      for (const userId of await userIdsP) {
+        const detail = getDetail({ userId, actionId: action.id });
+        if (
+          computeIsAwayDuringAnyOfLastMemberAction({
+            user: userCachedFilter.filtered({ id: userId })[0]!,
+            action,
+          }) &&
+          (!action.useManualCohort || action.manualCohortUserIdSet?.has(userId))
+        ) {
+          detail.status = UserActionRelationPillStatus.Away;
+        }
       }
     }
 
@@ -2284,7 +2296,9 @@ export class ActionsService {
   }
 
   async findUserActionRelations(): Promise<UserActionRelationsResponseDto> {
-    const usersPromise = this.userService.findAll();
+    const usersPromise = this.userService.findAll({
+      awayRanges: true,
+    });
     return this.findActionRelationsForUsers(usersPromise);
   }
 
@@ -2293,7 +2307,7 @@ export class ActionsService {
   ): Promise<CommunityUserInfoDto> {
     const usersPromise = this.userService
       .findCommunityOrFail(communityId, {
-        users: true,
+        users: { awayRanges: true },
       })
       .then((community) => community.users);
     return this.findActionRelationsForUsers(usersPromise);
@@ -2302,7 +2316,7 @@ export class ActionsService {
   async findMemberInfo(userId: number): Promise<CommunityUserInfoDto> {
     const usersPromise = this.userService
       .findCommunityForUserOrFail(userId, {
-        users: true,
+        users: { awayRanges: true },
       })
       .then((community) => community.users);
     return this.findActionRelationsForUsers(usersPromise);
