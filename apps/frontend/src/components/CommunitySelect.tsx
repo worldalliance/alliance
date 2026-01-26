6ee898cada64b0adaba06877a3575cd95c5275e7
Charles Lien <charleslien97@gmail.com>
2026-01-26 14:16:40 -0800

    Add leave group button in groups tab

--- apps/frontend/src/components/CommunitySelect.tsx
+++ apps/frontend/src/components/CommunitySelect.tsx
@@ -1,13 +1,16 @@
-import { CommunityDto } from "@alliance/shared/client";
+import { CommunityDto, userLeaveCommunity } from "@alliance/shared/client";
 import List from "@alliance/sharedweb/ui/List";
 import Button, { ButtonColor } from "@alliance/sharedweb/ui/Button";
 import { useAuth } from "../lib/AuthContext";
 import { Plus } from "lucide-react";
+import { useCallback, useMemo } from "react";
+import { useToast } from "@alliance/sharedweb/ui/ToastProvider";
+import React from "react";
 
 export type CommunitySelectProps = {
   communities: CommunityDto[] | null;
   currentCommunityId?: number | null;
-  onSelectCommunity: (communityId: number) => void;
+  onSelectCommunity: (communityId: number | null) => void;
   isOnboardingGroupMember: boolean;
   onCreateCommunity: () => void;
 };
@@ -21,6 +24,40 @@ const CommunitySelect = ({
 }: CommunitySelectProps) => {
   const { user } = useAuth();
 
+  const { leaderCommunities, nonLeaderCommunities } = useMemo(() => {
+    return {
+      leaderCommunities:
+        communities?.filter((community) =>
+          community.leaders.some((leader) => leader.id === user?.id)
+        ) ?? [],
+      nonLeaderCommunities:
+        communities?.filter(
+          (community) =>
+            !community.leaders.some((leader) => leader.id === user?.id)
+        ) ?? [],
+    };
+  }, [communities, user?.id]);
+
+  const { confirm } = useToast();
+
+  const onLeaveGroup = useCallback(
+    async (community: CommunityDto, anchor: HTMLElement | null) => {
+      const ok = await confirm({
+        title: `Leave group? (${community.name})`,
+        message: `Are you sure you want to leave this group? You will not be able to rejoin unless you are invited again.`,
+        anchorEl: anchor,
+        confirmLabel: "Leave",
+        cancelLabel: "Cancel",
+        placement: "topleft",
+      });
+      if (ok) {
+        await userLeaveCommunity({ path: { communityId: community.id } });
+        onSelectCommunity(null);
+      }
+    },
+    [confirm, onSelectCommunity]
+  );
+
   if ((!communities || communities.length === 0) && isOnboardingGroupMember) {
     return (
       <div className="py-4">
@@ -31,51 +68,91 @@ const CommunitySelect = ({
 
   return (
     <div className="flex flex-col gap-y-3 py-4">
-      <p className="font-semibold text-xl md:text-2xl">
-        Select a group to view
-      </p>
-      <List>
-        {[
-          ...(communities?.map((community) => {
-            const isLeader = community.leaders.some(
-              (leader) => leader.id === user?.id
-            );
+      {!!(leaderCommunities.length || !isOnboardingGroupMember) && (
+        <>
+          <p className="font-semibold text-xl md:text-2xl">Leader groups</p>
+          <List>
+            {[
+              ...(leaderCommunities.map((community) => {
+                const isCurrent = community.id === currentCommunityId;
+                return (
+                  <Button
+                    key={community.id}
+                    color={
+                      isCurrent ? ButtonColor.LightHover : ButtonColor.White
+                    }
+                    className="w-full !rounded-none"
+                    onClick={() => onSelectCommunity(community.id)}
+                  >
+                    <div className={"w-full flex flex-row justify-between m-2"}>
+                      <div className="flex flex-col gap-y-1 text-left">
+                        <p className="text-xl font-semibold">
+                          {community.name}
+                        </p>
+                        <p className="text-zinc-500">{community.description}</p>
+                        <span className="text-zinc-500">
+                          {community.users.length}{" "}
+                          {community.users.length === 1 ? "member" : "members"}
+                        </span>
+                      </div>
+                    </div>
+                  </Button>
+                );
+              }) ?? []),
+              !isOnboardingGroupMember && (
+                <Button
+                  key="create"
+                  onClick={onCreateCommunity}
+                  color={ButtonColor.White}
+                  className="w-full !rounded-none"
+                >
+                  <div className="w-full flex flex-row gap-x-2 items-center justify-center m-3 text-zinc-500">
+                    <Plus size="14" /> Create a new group
+                  </div>
+                </Button>
+              ),
+            ]}
+          </List>
+        </>
+      )}
+      {!!nonLeaderCommunities.length && (
+        <>
+          <p className="font-semibold text-xl md:text-2xl">Member groups</p>
+          {nonLeaderCommunities.map((community) => {
             const isCurrent = community.id === currentCommunityId;
             return (
-              <Button
-                key={community.id}
-                color={isCurrent ? ButtonColor.LightHover : ButtonColor.White}
-                className="w-full !rounded-none"
-                onClick={() => onSelectCommunity(community.id)}
-              >
-                <div className={"w-full flex flex-row justify-between m-2"}>
-                  <div className="flex flex-col gap-y-1 text-left">
-                    <p className="text-xl font-semibold">{community.name}</p>
-                    <p className="text-zinc-500">{community.description}</p>
-                    <span className="text-zinc-500">
-                      {community.users.length}{" "}
-                      {community.users.length === 1 ? "member" : "members"}
-                    </span>
+              <React.Fragment key={community.id}>
+                <Button
+                  color={isCurrent ? ButtonColor.LightHover : ButtonColor.White}
+                  className="w-full !rounded-none"
+                  onClick={() => onSelectCommunity(community.id)}
+                >
+                  <div className={"w-full flex flex-row justify-between m-2"}>
+                    <div className="flex flex-col gap-y-1 text-left">
+                      <p className="text-xl font-semibold">{community.name}</p>
+                      <p className="text-zinc-500">{community.description}</p>
+                      <span className="text-zinc-500">
+                        {community.users.length}{" "}
+                        {community.users.length === 1 ? "member" : "members"}
+                      </span>
+                    </div>
                   </div>
-                  {isLeader && <span className="text-green">Leader</span>}
+                </Button>
+                <div className="w-full flex flex-row justify-end">
+                  <Button
+                    color={ButtonColor.Red}
+                    onClick={(event) =>
+                      void onLeaveGroup(community, event.currentTarget)
+                    }
+                  >
+                    Leave group
+                  </Button>
                 </div>
-              </Button>
+              </React.Fragment>
             );
-          }) ?? []),
-          !isOnboardingGroupMember && (
-            <Button
-              key="create"
-              onClick={onCreateCommunity}
-              color={ButtonColor.White}
-              className="w-full !rounded-none"
-            >
-              <div className="w-full flex flex-row gap-x-2 items-center justify-center m-3 text-zinc-500">
-                <Plus size="14" /> Create a new group
-              </div>
-            </Button>
-          ),
-        ]}
-      </List>
+          })}
+        </>
+      )}
     </div>
   );
 };
