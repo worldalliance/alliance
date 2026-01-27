b1ca03f6885153e943822cc3d868b348dbaa0fd3
Charles Lien <charleslien97@gmail.com>
2026-01-27 15:26:23 -0800

    fix grammatical error

--- shared/lib/copy.ts
+++ shared/lib/copy.ts
@@ -31,6 +31,6 @@ export const requestGroupAssignmentConfirmation =
   "Are you sure you want to be reassigned to a different group? During this process, you will be removed from your current group. Note that this process may take a few days.";
 
 export const editGroupPublicGroupExplanation =
-  "Public groups can be joined by anyone without your approval. All public also enable group assignment.";
+  "Public groups can be joined by anyone without your approval.";
 export const editGroupGroupAssignmentExplanation =
   "Enabling group assignment means that members of other groups may occasionally be assigned to this group by staff. Staff will not assign members to this group past the capacity.";
