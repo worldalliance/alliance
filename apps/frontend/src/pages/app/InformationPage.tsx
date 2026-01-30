b2fa1f8d76c58a05664b6a9e9d6ab60155f7f891
sidsquid <sidney.hough@gmail.com>
2026-01-29 21:25:55 -0800

    very simple initial roadmap

--- apps/frontend/src/pages/app/InformationPage.tsx
+++ apps/frontend/src/pages/app/InformationPage.tsx
@@ -41,6 +41,59 @@ const InformationPage: React.FC = () => {
           </ResourceButton>
         </div>
 
+        <div className="flex flex-col mt-4">
+          <h2 className="text-2xl font-semibold">Roadmap</h2>
+          <p className="text-zinc-500">Last updated 1/29/2026</p>
+        </div>
+        <div className="flex flex-col gap-y-2">
+          <p>
+            We are in an experimental, invite-only phase. We are working up to a
+            public launch in which we will allow anyone to join the Alliance. In
+            general, this preparation entails:
+          </p>
+
+          <ul className="list-disc list-inside">
+            <li>
+              <span className="font-semibold">
+                Ensuring that our action production process is smooth and
+                reliable.
+              </span>{" "}
+              This includes understanding what makes actions effective and,
+              eventually, hiring experts in fields relevant to our priorities.
+            </li>
+            <li>
+              <span className="font-semibold">
+                Building organizational structures and processes that help
+                members contribute reliably and straightforwardly.
+              </span>{" "}
+              This includes designing Alliance groups and determining our
+              governance procedures.
+            </li>
+            <li>
+              <span className="font-semibold">
+                Developing a functional and accessible online platform.
+              </span>{" "}
+              This includes improving our platform based on early members&apos;
+              feedback and testing it with a wider, more global audience.
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-col mt-4">
+          <p>Right now, the office is:</p>
+          <ul className="list-disc list-inside">
+            <li>
+              Streamlining the process of leading and joining groups so that we
+              can accommodate more members.
+            </li>
+            <li>Developing a mobile app.</li>
+            <li>
+              Moving important notifications to the homepage for greater
+              visibility.
+            </li>
+            <li>Hiring and fundraising.</li>
+          </ul>
+        </div>
+
         <h2 className="text-2xl font-semibold mt-4">Action updates</h2>
 
         <div className="flex flex-col gap-y-4 text-base">
